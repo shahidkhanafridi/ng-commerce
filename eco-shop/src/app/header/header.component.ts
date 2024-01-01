f617f2f35d53e6731cc670194acaed3b11f3ada2
shahidkhanafridi <muhammadshahidkhanafridi@gmail.com>
2024-01-01 19:17:16 +0500

    Dynamically change navbar based on user need

--- eco-shop/src/app/header/header.component.ts
+++ eco-shop/src/app/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  menuType: string = 'default';
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.events.subscribe((val: any) => {
+      if (val.url) {
+        console.log(val.url);
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.log('this is seller area');
+          this.menuType="seller";
+        } else {
+          console.log('outside seller area');
+          this.menuType="default";
+        }
+      }
+    });
   }
-
 }
