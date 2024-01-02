46d2e40bd80161857efc521105a4ce7e04259d59
shahidkhanafridi <muhammadshahidkhanafridi@gmail.com>
2024-01-02 19:20:21 +0500

    Addign product APi and backend functionality is completed

--- eco-shop/src/app/seller-add-product/seller-add-product.component.ts
+++ eco-shop/src/app/seller-add-product/seller-add-product.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.css']
+  styleUrls: ['./seller-add-product.component.css'],
 })
 export class SellerAddProductComponent implements OnInit {
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  submit(data:any){
+  ngOnInit(): void {}
+  submit(data: product) {
     console.log(data);
+    this.product.addProduct(data).subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.addProductMessage = 'Product is added successfully';
+      }
+    });
+    setTimeout(() => {
+      this.addProductMessage = undefined;
+    }, 3000);
   }
 }
