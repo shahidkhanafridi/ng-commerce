92917a31f61fa170702faeed7e577aa23a154abc
shahidkhanafridi <muhammadshahidkhanafridi@gmail.com>
2024-01-03 18:55:17 +0500

    Geting data to update component and populating data in the form using get product service completed

--- eco-shop/src/app/seller-update-product/seller-update-product.component.ts
+++ eco-shop/src/app/seller-update-product/seller-update-product.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { product } from '../data-type';
 import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-seller-update-product',
   templateUrl: './seller-update-product.component.html',
-  styleUrls: ['./seller-update-product.component.css']
+  styleUrls: ['./seller-update-product.component.css'],
 })
 export class SellerUpdateProductComponent implements OnInit {
   addProductMessage: string | undefined;
+  productData: undefined | product;
 
-  constructor(private product: ProductService) { }
+  constructor(private route: ActivatedRoute, private product: ProductService) {}
 
   ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    console.log(productId);
+    productId &&
+      this.product.getProduct(productId).subscribe((data) => {
+        console.log(data);
+        this.productData = data;
+      });
   }
   submit(data: product) {
     console.log(data);
@@ -26,5 +35,4 @@ export class SellerUpdateProductComponent implements OnInit {
       this.addProductMessage = undefined;
     }, 3000);
   }
-
 }
